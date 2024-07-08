165152aa9e383720d0847ed3503d1dfec3643dd7
alex <sasza262626@gmail.com>
2024-07-08 20:03:28 +0200

    add index file

--- js/task-3.js
+++ js/task-3.js
@@ -5,74 +5,37 @@ console.log(
     {
       name: "Moore Hensley",
       friends: ["Sharron Pace"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Sharlene Bush",
       friends: ["Briana Decker", "Sharron Pace"],
-      gender: "female"
+      gender: "female",
     },
     {
       name: "Ross Vazquez",
       friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Elma Head",
       friends: ["Goldie Gentry", "Aisha Tran"],
-      gender: "female"
+      gender: "female",
     },
     {
       name: "Carey Barr",
       friends: ["Jordan Sampson", "Eddie Strong"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Blackburn Dotson",
       friends: ["Jacklyn Lucas", "Linda Chapman"],
-      gender: "male"
+      gender: "male",
     },
     {
       name: "Sheree Anthony",
       friends: ["Goldie Gentry", "Briana Decker"],
-      gender: "female"
-    }
+      gender: "female",
+    },
   ])
 );
-// [
-//   {
-//     name: "Ross Vazquez",
-//     friends: ["Marilyn Mcintosh", "Padilla Garrison", "Naomi Buckner"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     friends: ["Briana Decker", "Sharron Pace"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Elma Head",
-//     friends: ["Goldie Gentry", "Aisha Tran"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Carey Barr",
-//     friends: ["Jordan Sampson", "Eddie Strong"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     friends: ["Jacklyn Lucas", "Linda Chapman"],
-//     gender: "male"
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     friends: ["Goldie Gentry", "Briana Decker"],
-//     gender: "female"
-//   },
-//   {
-//     name: "Moore Hensley",
-//     friends: ["Sharron Pace"],
-//     gender: "male"
-//   }
-// ]
